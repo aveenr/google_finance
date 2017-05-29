74a1fdecf891a39de39b17eed8477a13898fc2a4
aveenr <aveenr@gmail.com>
2017-05-29 11:59:48 +0200

    Update google_finance.py
    Loop with multiple pre-defined symbols

--- google_finance.py
+++ google_finance.py
@@ -1,69 +1,46 @@
 from bs4 import BeautifulSoup
 import requests
+from time import localtime, strftime
 
-import urllib
+# INIT JSE CODE AND LINKS
+SYMBOLS = 'bil'
+BASE_SYMBOLS = ['oml','npn','agl']
+GOOGLE_FINANCE_URL = 'https://www.google.com/finance'
+GOOGLE_FINANCE_HISTORICAL_URL = 'https://www.google.com/finance/historical'
+# code_url = GOOGLE_FINANCE_URL + '?q=JSE%3A' + SYMBOLS
+YEARS = 20
 
-# INPUT JSE CODE
-x_code = ['NPN','OML','SHP']
-goog_finc = 'https://www.google.com/finance'
-goog_finc_hist = 'https://www.google.com/finance/historical'
-
-code=[]
-
-for code in x_code:
-    code_url = goog_finc+'?q=JSE%3A'+code
-
+for SYMBOLS in BASE_SYMBOLS:
     # OPEN JSE CODE PAGE IN GOOGLE FINANCE
+    code_url = GOOGLE_FINANCE_URL + '?q=JSE%3A' + SYMBOLS
     request = requests.get(code_url)
     content = request.content
-    soup = BeautifulSoup(content,'html.parser')
-    parsed = soup.find_all('link',{'rel':'canonical'})
-
+    soup = BeautifulSoup(content, 'html.parser')
+    parsed = soup.find_all('link', {'rel': 'canonical'})
+    
     # FIND CID CODE
     for cid in parsed:
-        # CID LINK HREF LINK, FORMAT TO KEEP THE CODE
+        # HARDCODED FOR CID ID
         cid_code = cid['href'][34:]
-        print(cid_code)
-        # yolo = x['href'][34:]
-
-    # GET HISTORY PAGE
-    # code_history_url = ('https://www.google.com/finance/historical?cid=' + cid +
-    #                         '&startdate=' + 'start_date=' + '&enddate=' + end_date)
-
-    start_month = 'Mar+'
-    start_day = '1%2C+'
-    start_year = '2017'
-    start_date = start_month + start_day + start_year
-    print(start_date)
-
-    end_month = 'Mar+'
-    end_day = '23%2C+'
-    end_year = '2017'
-    end_date = end_month + end_day + end_year
-    print(end_date)
-
-
-    # a = (goog_finc_hist + '?cid=' + cid_code + '&startdate=' + start_date + '&enddate=' + end_date)
-    a = (goog_finc_hist + '?cid=' + cid_code + '&startdate=' + start_date + '&enddate=' + end_date + '&output=csv')
-
-    request = requests.get(a)
-    content = request.content
-    soup = BeautifulSoup(content,'html.parser')
-
-    print(soup)
-
-    with open(code+'.csv', 'wb') as handle:
-        response = requests.get(a, stream=True)
+    
+    # INIT DATE PARAMS
+    # Mar+1%2C+2000
+    back_year = ((int(strftime('%Y')) - YEARS))
+    start_date = (strftime('%b+' + '%d' + '%%2C+')) + str(back_year)
+    end_date = (strftime('%b+' + '%d' + '%%2C+' + '%Y'))
+    
+    # OUTPUT DOWNLOAD LINK
+    historical_url = (GOOGLE_FINANCE_HISTORICAL_URL + '?cid=' + cid_code + '&startdate=' + start_date + '&enddate=' + end_date + '&output=csv')
+    
+    # OUPUT TO SCREEN
+    # request = requests.get(a)
+    # content = request.content
+    # soup = BeautifulSoup(content, 'html.parser')
+    
+    # DOWNLOAD TO LOCAL DIRECTORY
+    with open(SYMBOLS.lower() + '.csv', 'wb') as handle:
+        response = requests.get(historical_url, stream=True)
         if not response.ok:
             print('File could not be downloaded')
         for block in response.iter_content(1024):
             handle.write(block)
-
-# http://www.google.com/finance/historical?cid=187562520823645&startdate=Mar23%2C2010&enddate=Mar22%2C2017
-# http://www.google.com/finance/historical?cid=187562520823645&startdate=Mar23%2C2010&enddate=Mar22%2C2017&output=csv
-
-# DOWNLOAD
-
-
-# code_history_url = ('https://www.google.com/finance/historical?cid=' + cid +
-#                     '&startdate=' + 'start_date=' + '&enddate=' + end_date)
