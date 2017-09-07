f25b5bce560f49cd3ebe63041a242785b6bd9757
Aveen Ramchurren <aveenr@gmail.com>
2017-09-07 10:21:55 +0200

    Update google_finance.py
    added command line arguments
    minor refactoring

--- google_finance.py
+++ google_finance.py
@@ -1,31 +1,29 @@
 import requests
-from time import strftime
+# from time import strftime
 import logging
 import os
 import errno
 import datetime
 from datetime import date
+import argparse
 
-class myClass(object):
-    def __init__(self, period=5, data_file_name='stocks.txt', dl_path='Github'):
+class GoogleFinanceDownload(object):
+    """ Google Finance Downloader that downloads historical data for a list of stocks """
+    def __init__(self, period, data_file_name, dl_path):
         self.url = 'https://www.google.com/finance/historical'
         self.period = period
         self.file = data_file_name
         self.dl_path = dl_path
-
-        # root_dir = './'
-        # dl_dir = 'git'
-        # self.dl_path = os.path.join(root_dir, dl_dir)
-
         self.invalid = []
         self.stocks = []
 
     def get_stocks(self):
+        """" Get stocks data from text file """
         try:
             with open(self.file, 'r') as data_file:
                 self.stocks = (data_file.read()).split()
         except IOError:
-            print('ERROR: File Not found')
+            print('ERROR: {0} File Not found'.format(self.file))
             exit(1)
         logging.info('Contents of data file %s : ',self.stocks)
         self.make_dir()  #directory created for downloads
@@ -36,6 +34,7 @@ class myClass(object):
         print(self.__dict__)
 
     def make_dir(self):
+        """ Creates the destination directory """
         try:
             os.makedirs(self.dl_path)
             os.chdir(self.dl_path) #make directory and change to it
@@ -47,15 +46,17 @@ class myClass(object):
                 raise  # something else happened
 
     def logger_name(self):
-        return (datetime.datetime.today().strftime('z%Y%m%d_%H%M%S'))+'.log'
+        return (datetime.datetime.today().strftime('%Y%m%d_%H%M%S'))+'.log'
 
     def query_string_date(self, years=0):
+        """" Create date string for url """
         Y = date.today().year
         M = date.today().month
         D = date.today().day
         return (date(Y - years, M, D).strftime('%b %d %Y'))
 
-    def download_historical_data(self, stock):
+    def download_csv(self, stock):
+        """" Downloads csv file from Google Finance """
         logging.info('Current stock is %s',stock.upper())
         historical_payload = {'q':'jse:'+stock,'output':'csv',
                              'startdate':self.query_string_date(self.period),
@@ -76,20 +77,29 @@ class myClass(object):
                 print(stock.upper() + ' is complete')
 
 def main():
-    JSE_Download = myClass(dl_path='myJse', period=1)
+    parser = argparse.ArgumentParser(description='Google Finance Downloader: Downloads historical data of list of stocks as csv files')
+    parser.add_argument('-f', '--filename', help='Path to file containing stocks to download', default='stocks.txt')
+    ## feature to add parser.add_argument('--exchange', '-e', help='Name of exchange (default=JSE', nargs='*', default='JSE')
+    parser.add_argument('-p', '--path', help='Directory name for download', default='stocks')
+    parser.add_argument('-t', '--time', help='Years to count back (default = 1 year)', type=int, default=1)
+    args = parser.parse_args()
+
+    # init class
+    JSE_Download = GoogleFinanceDownload(period=args.time, data_file_name=args.filename, dl_path=args.path)
+
+    # init logger
     logging.basicConfig(filename=JSE_Download.logger_name(),
                     level=logging.INFO,
                     format='%(asctime)s %(message)s',
                     datefmt='%Y/%m/%d %I:%M:%S %p')
-
+    # main
     for stock in JSE_Download.get_stocks():
         try:
-            JSE_Download.download_historical_data(stock)
+            JSE_Download.download_csv(stock)
         except requests.exceptions.RequestException as e:
             logging.info('Error in connection %s ', e)
             break
-        logging.info('%s', JSE_Download.invalid)
-    # JSE_Download.class_dump()
+    logging.info('%s', JSE_Download.invalid)
 
 if __name__ == '__main__':
     main()
